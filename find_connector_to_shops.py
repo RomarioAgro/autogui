97c9a8bfb22bffdb9f6e584c104dcf3fa2012c69
Кожин Роман <resume-gl@yandex.ru>
2022-08-06 22:43:27 +0300

    потестировал поиск рабочих файлов в папке с магазинами, шелэксекут

---
+++ find_connector_to_shops.py
@@ -0,0 +1,27 @@
+import os
+from typing import Iterable
+
+
+def gen_files_path(path_work: str, find_o: str, list_dir_magaz: list) -> Iterable[str]:
+    """
+    функция-генератор рекурсивно обходит рабочую папку и ищет в ней нужный файл
+    :param path_work: str рабочая папка которую обходим
+    :param find_o: str файл которую ищем
+    """
+    for base_path, sub_path, files_in_path in os.walk(path_work):
+        if find_o in files_in_path:
+            list_dir_magaz.append(base_path)
+        else:
+            yield base_path
+
+
+def main():
+    i_path = 'D:\\YandexDisk\\rdp\\Dostup_magazini\\'
+    file_n = 'GS_magaz.BAT'
+    list_dir_magaz = []
+    for _ in gen_files_path(i_path, file_n, list_dir_magaz):
+        pass
+    print(list_dir_magaz)
+
+if __name__ == '__main__':
+    main()
--- main.py
+++ main.py
@@ -1,24 +1,31 @@
 import pyautogui
-from sys import exit
-
-pyautogui.PAUSE = 1.5
-xy = pyautogui.position()
-# pyautogui.alert('ййййй')
-log_rdp = pyautogui.locateOnScreen('logon_rdp_darc.png')
-if log_rdp is None:
-    exit(0)
-print(log_rdp)
-pyautogui.leftClick(log_rdp[0] + 30, log_rdp[1] // 2, 1)
-pyautogui.write('123')
-pyautogui.press('enter')
-
-while True:
-    logon_sbis = pyautogui.locateOnScreen('logon_sbis.png')
-    print(logon_sbis)
-    if logon_sbis is not None:
-        break
-pyautogui.write('ADMIN')
-pyautogui.press('enter')
-pyautogui.write('FDNJRHFNBZ')
-pyautogui.press('enter')
-
+import win32api
+# from sys import exit
+# import os
+# from typing import Iterable
+# from find_connector_to_shops import gen_files_path
+#
+# pyautogui.PAUSE = 1.5
+# xy = pyautogui.position()
+# log_rdp = pyautogui.locateOnScreen('logon_rdp_darc.png')
+# if log_rdp is None:
+#     exit(0)
+# print(log_rdp)
+# pyautogui.leftClick(log_rdp[0] + 30, log_rdp[1] // 2, 1)
+# pyautogui.write('123')
+# pyautogui.press('enter')
+#
+# while True:
+#     logon_sbis = pyautogui.locateOnScreen('logon_sbis.png')
+#     print(logon_sbis)
+#     if logon_sbis is not None:
+#         break
+# pyautogui.write('ADMIN')
+# pyautogui.press('enter')
+# pyautogui.write('FDNJRHFNBZ')
+# pyautogui.press('enter')
+#
+# win32api.shel('D:\\YandexDisk\\rdp\\Dostup_magazini\\ace\\Clients\\GS_magaz.BAT')
+i_path = 'D:\\YandexDisk\\rdp\\Dostup_magazini\\ace\\Clients\\GS_magaz.BAT'
+work_parh = 'D:\\YandexDisk\\rdp\\Dostup_magazini\\ace\\Clients\\'
+win32api.ShellExecute(0, 'open', i_path, None, work_parh, 1)
